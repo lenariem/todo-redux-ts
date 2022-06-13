c474f7b94a582952f295efc7ad3950522e4455fb
unknown <olena.denysova88@gmail.com>
2022-06-13 18:33:01 +0200

    users list

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, {useEffect} from 'react';
+import {useTypedSelector} from "../hooks/useTypedSelector";
+import {fetchUsers} from "../store/action-creators/user";
+import {useActions} from "../hooks/useActions";
 
-export const UserList: React.FC = () => {
-    const state = useSelector(state => state);
-    console.log(state);
-    
-  return (
-    <div>: React.FC</div>
-  )
-}
+const UserList: React.FC = () => {
+    const {users, error, loading} = useTypedSelector(state => state.user)
+    const {fetchUsers} = useActions()
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <h1>{error}</h1>
+    }
+
+    return (
+        <div>
+            {users.map(user =>
+                <div key={user.id}>{user.name}</div>
+            )}
+        </div>
+    );
+};
+
+export default UserList;
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -5,6 +5,7 @@ import {userReducer} from "./userReducer";
 
 export const rootReducer = combineReducers({
     user: userReducer,
+    
    
 })
 
